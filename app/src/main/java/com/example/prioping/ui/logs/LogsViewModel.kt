cfaac9c0b81f7bdb428e8da896911645798d4f67
lukas <lukas.petersson.1999@gmail.com>
2023-06-01 09:35:02 +0200

    app running

--- app/src/main/java/com/example/prioping/ui/logs/LogsViewModel.kt
+++ app/src/main/java/com/example/prioping/ui/logs/LogsViewModel.kt
@@ -1,11 +1,9 @@
 package com.example.prioping.ui.logs
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
 import com.example.prioping.service.NotificationService
 
 class LogsViewModel : ViewModel() {
 
-    val notifications = NotificationService.notifications.asLiveData()
+    val notifications = NotificationService.notifications
 }
-
